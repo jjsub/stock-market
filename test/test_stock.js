83a2e4aef828bb9d285ef1a1780653f468635ca5
Juan <jsamesub@gmail.com>
2014-08-04 13:35:32 -0500

    testing

---
+++ test/test_stock.js
@@ -0,0 +1,29 @@
+/* globals describe, it */
+
+'use strict';
+
+var expect = require('chai').expect;
+var Stock = require('../app/models/stock');
+
+
+describe('Stock', function(){
+  describe('constructor', function(){
+    it('should create a new stock object', function(){
+      var stock = new Stock('aapl');
+
+      expect(stock).to.be.instanceOf(Stock);
+      expect(stock.symbol).to.equal('AAPL');
+    });
+  });
+
+  describe('#getQuote', function(){
+    it('should update the stock\'s price', function(){
+      var stock = new Stock('aapl');
+      stock.getQuote(function(){
+        expect(stock.price).to.be.at.least(0);
+        done(); 
+      });
+    });
+  });
+});
+
