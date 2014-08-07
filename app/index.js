5ba973f229b120aaabd1e5e46d7cad0703158500
jjsub <jsamesub@gmail.com>
2014-08-08 01:07:18 +0900

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Stock = require('./models/stock');
+var Stock = require('./models/stock');      // This is the requiere os the export ( module.exports = Stock;) from the module Stock//
 
 var stock = new Stock('fb');
 
